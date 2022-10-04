2dd9e6a75347b86a5fbf6c8cd25a0dce37b1a62c
starboy77 <ayomidemajoyegbe@gmail.com>
2022-10-04 04:29:35 +0100

    2=arguments file updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const len = process.argv.length;
-if (len > 2) console.log('Argument found');
+if (len === 3) console.log('Argument found');
+else if (len > 3) console.log('Arguments found');
 else console.log('No argument');
